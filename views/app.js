e40625c3ce6d296984963ccd4666b094b6699c96
Dzmitry Dubrovin <dzmitry_dubrovin@senla.eu>
2016-12-04 12:34:39 +0300

    correctly resolve project estimation state

--- views/app.js
+++ views/app.js
@@ -46,6 +46,24 @@ app.config([
 app.run(function($rootScope, $state, StatusService) {
     $rootScope.$state = $state;
 
+    $rootScope.$on('$locationChangeStart', function (e, newURL, oldURL) {
+
+        // if user reload page
+        if(oldURL === newURL) {
+
+            // for some reason location like 'project/MEDF/estimation/MEDF-64323'
+            // detects as invalid by $location-service
+            // so try to detect this and use appropriate state
+            var urlKeys = newURL.split('/');
+            var sharpPos = urlKeys.indexOf('#');
+            urlKeys = urlKeys.splice(sharpPos + 1, urlKeys.length - sharpPos);
+            if(urlKeys.length === 4 && urlKeys[0] === 'project' && urlKeys[2] === 'estimation') {
+                e.preventDefault();
+                $state.go('projectEstimation', {projectKey: urlKeys[1], key: urlKeys[3]});
+            }
+        }
+    });
+
     $rootScope.$on('$stateChangeSuccess', function(e, toState) {
         if (toState.data) {
             document.title = toState.data.title;
